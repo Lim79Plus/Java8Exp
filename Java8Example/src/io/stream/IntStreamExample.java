f2ce7c211d589a23d4eb2e137c242d16bcb21a97
Lim79Plus <masae.h79@gmail.com>
2017-12-28 22:29:04 +0900

    added another code

--- Java8Example/src/io/stream/IntStreamExample.java
+++ Java8Example/src/io/stream/IntStreamExample.java
@@ -1,12 +1,25 @@
 package io.stream;
 
+import java.util.OptionalDouble;
+import java.util.OptionalInt;
 import java.util.stream.IntStream;
 
 public class IntStreamExample {
 
 	public static void main(String[] args) {
 
-		IntStream.range(0, 10).forEach(System.out::println);
+		//print IntStream.range[0123456789]
+		System.out.print("IntStream.range[");
+		IntStream.range(0, 10).forEach(System.out::print);
+		System.out.println("]");
+		
+		//print OptionalDouble[4.5]
+		OptionalDouble result = IntStream.range(0, 10).average();
+		System.out.println(result);
+		
+		//print OptionalInt[9]
+		OptionalInt result2 = IntStream.range(0, 10).max();
+		System.out.println(result2);
 	}
 
 }
