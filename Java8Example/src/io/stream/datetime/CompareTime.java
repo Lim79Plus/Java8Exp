b161a21652672bcda923b9fdc0918d8f466118aa
Lim79Plus <masae.h79@gmail.com>
2018-01-13 22:41:46 +0900

    CompareTime

---
+++ Java8Example/src/io/stream/datetime/CompareTime.java
@@ -0,0 +1,23 @@
+package io.stream.datetime;
+
+import java.time.LocalDateTime;
+
+public class CompareTime {
+
+	public static void main(String[] args) {
+
+		LocalDateTime dt = LocalDateTime.now();
+		print(dt);//show now date
+		
+		LocalDateTime oldDt = dt.minusYears(1).minusMonths(2).minusDays(3);
+		print(oldDt);// show old date
+		print(dt.isAfter(oldDt)); //true
+		print(dt.isBefore(oldDt)); //false
+		
+	}
+	
+	private static void print(Object obj){
+		System.out.println(obj);
+	}
+
+}
